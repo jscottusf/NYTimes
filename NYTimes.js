41cd424e30fab335828359e1492957c450af13bd
Andrea Labis <sweetlila111282@aol.com>
2020-01-21 21:34:49 -0500

    so far

--- NYTimes.js
+++ NYTimes.js
@@ -1,5 +1,5 @@
 
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=XKzeCKZfebLMl2q4GYfkjLt1rI3G8ofg";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTerm+"&api-key=XKzeCKZfebLMl2q4GYfkjLt1rI3G8ofg";
         
 
         $.ajax({
@@ -9,7 +9,7 @@
             console.log(response);
 
        var fieldName = $("<h1>").text(response.response.docs[0].title);
-       var  
+       var searchTerm = $("#search-term").attr()
        
 
 
